1a8fa9ebd17da93cd0e3107bb0e38a9c14a1c48a
sambr <yura@gmail.com>
2024-12-15 13:07:44 +0200

    homework7

---
+++ homework12.py
@@ -0,0 +1,8 @@
+def contains_hand(values):
+    result = 'hand' in values
+    print("Результат:", result)
+    return result
+
+
+values_list = ["foot", "eye", "hand", "ear"]
+contains_hand(values_list)
